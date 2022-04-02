f59e27163817d97af2aa7d42dc05eca8b300b236
M Haidar Hanif <git@mhaidarhanif.com>
2022-04-03 03:41:59 +0700

    feat: signup signin routes

---
+++ app/routes/signin.tsx
@@ -0,0 +1,7 @@
+export default function SignInRoute() {
+  return (
+    <div>
+      <h1>Welcome back! Let's sign in</h1>
+    </div>
+  );
+}
---
+++ app/routes/signup.tsx
@@ -0,0 +1,7 @@
+export default function SignUpRoute() {
+  return (
+    <div>
+      <h1>Create your new account</h1>
+    </div>
+  );
+}
