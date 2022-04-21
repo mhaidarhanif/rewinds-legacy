71313e71808d98bbf7748aa9a361982bfa45924f
M Haidar Hanif <git@mhaidarhanif.com>
2022-04-21 16:29:19 +0700

    docs: update homepage content

--- app/contents/intro.tsx
+++ app/contents/intro.tsx
@@ -1,12 +1,14 @@
 export const Intro = () => {
   return (
     <div className="prose dark:prose-invert">
-      <h1>Remix Tailwind Starter Kit</h1>
-      <h2>Remix starter kit with Tailwind family of libraries</h2>
+      <h1>Rewinds, a Remix Tailwind Starter Kit</h1>
       <p>
-        Example demo to combine the best Tailwind-related ecosystem such as
-        Tailwind CSS v3, Tailwind UI, Headless UI, Radix UI, Vechai UI, and
-        more.
+        <b>Rewinds</b> is a Remix starter kit with Tailwind family of libraries.
+      </p>
+      <p>
+        The goal is to have several examples and demos to combine the best
+        Tailwind-related ecosystem such as Tailwind CSS v3, Tailwind UI,
+        Headless UI, Radix UI, Vechai UI, and more.
       </p>
     </div>
   );
