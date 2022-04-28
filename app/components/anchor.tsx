a7745b9ba6049ccd233b34a65249ed385c90e59d
M Haidar Hanif <git@mhaidarhanif.com>
2022-04-28 20:37:34 +0700

    refactor: anchor props

--- app/components/anchor.tsx
+++ app/components/anchor.tsx
@@ -7,19 +7,24 @@ interface AnchorProps {
   href: string;
   children: React.ReactNode;
   className?: string;
+  underline?: boolean;
+  'data-color'?: string;
 }
 
 export const Anchor: FunctionComponent<AnchorProps> = ({
   href,
   children,
   className,
+  underline,
+  'data-color': dataColor,
 }) => {
   return (
     <a
-      className={classx('hover:underline', className)}
+      className={classx(underline && 'hover:underline', className)}
       href={href}
       target="_blank"
       rel="noreferrer"
+      data-color={dataColor}
     >
       {children}
     </a>
