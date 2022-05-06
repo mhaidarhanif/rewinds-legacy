ea23d4ed462b23f729e9e3ff73bf17f28c88200d
M Haidar Hanif <git@mhaidarhanif.com>
2022-05-06 12:52:06 +0700

    feat: configure pwa with service worker

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -12,3 +12,11 @@ if (ENV.NODE_ENV !== 'development') {
   // Splitbee Analytics
   splitbee.init();
 }
+
+// if the browser supports SW (all modern browsers do it)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    // we will register it after the page complete the load
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
