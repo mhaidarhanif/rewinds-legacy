6d32b413bdc43ae74cb0798841d7bb859153acd6
M Haidar Hanif <git@mhaidarhanif.com>
2022-05-08 08:13:01 +0700

    refactor: adjust entry client and dependencies

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,22 +1,26 @@
 import { RemixBrowser } from '@remix-run/react';
 import { hydrate } from 'react-dom';
 
+import { configMeta } from '~/configs';
 import { splitbee, ReactGA } from '~/libs';
 
 hydrate(<RemixBrowser />, document);
 
-if (ENV.NODE_ENV !== 'development') {
-  // Google Analytics
-  ReactGA.initialize(String(ENV.GA_MEASUREMENT_ID));
-
+if (
+  ENV.NODE_ENV !== 'development' &&
+  configMeta.url === window.location.hostname
+) {
   // Splitbee Analytics
   splitbee.init();
+
+  // Google Analytics
+  ReactGA.initialize(String(ENV.GA_MEASUREMENT_ID));
 }
 
-// if the browser supports SW (all modern browsers do it)
+// If the browser supports Service Worker API
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    // we will register it after the page complete the load
+    // Register after the page complete the load
     navigator.serviceWorker.register('/sw.js');
   });
 }
--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,12 @@
+/**
+ * Service Worker for PWA
+ */
+
 self.addEventListener('fetch', (event) => {
   let url = new URL(event.request.url);
   let method = event.request.method;
 
-  // any non GET request is ignored
+  // Any non GET request is ignored
   if (method.toLowerCase() !== 'get') return;
 
   // If the request is for the favicons, fonts, or the built files (which are hashed in the name)
@@ -12,14 +16,14 @@ self.addEventListener('fetch', (event) => {
     url.pathname.startsWith('/build/')
   ) {
     event.respondWith(
-      // we will open the assets cache
+      //Open the assets cache
       caches.open('assets').then(async (cache) => {
-        // if the request is cached we will use the cache
+        // If the request is cached we will use the cache
         let cacheResponse = await cache.match(event.request);
         if (cacheResponse) return cacheResponse;
 
-        // if it's not cached we will run the fetch, cache it and return it
-        // this way the next time this asset it's needed it will load from the cache
+        // If it's not cached we will run the fetch, cache it and return it
+        // This way the next time it's needed it will load from the cache
         let fetchResponse = await fetch(event.request);
         cache.put(event.request, fetchResponse.clone());
 
@@ -28,23 +32,23 @@ self.addEventListener('fetch', (event) => {
     );
   }
 
-  // if the URL is for a localized message
+  // If the URL is for a localized message
   if (url.pathname.startsWith('/locales/')) {
     event.respondWith(
-      // we will use the locales cache
+      // Use the locales cache
       caches.open('locales').then(async (cache) => {
-        // we will run the fetch
+        // Run the fetch
         let fetchResponsePromise = fetch(event.request);
         try {
-          // try to read from cache
+          // Try to read from cache
           let cacheResponse = await cache.match(event.request);
           if (cacheResponse) return cacheResponse;
         } finally {
-          // and finally if it was not cached or after we sent the response
+          // Finally if it was not cached or after we sent the response
           let fetchResponse = await fetchResponsePromise;
-          // we will update the cache
+          // Update the cache
           cache.put(event.request, fetchResponse.clone());
-          // and return the response
+          // Return the response
           return fetchResponse;
         }
       }),
