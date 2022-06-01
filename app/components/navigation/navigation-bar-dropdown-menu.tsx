d4176d72f30b009a5508db230ac11dfe14ec85cc
M Haidar Hanif <git@mhaidarhanif.com>
2022-06-02 03:24:29 +0700

    refactor: dropdown menu sub components

--- app/components/navigation/navigation-bar-dropdown-menu.tsx
+++ app/components/navigation/navigation-bar-dropdown-menu.tsx
@@ -1,43 +1,16 @@
 /* eslint-disable react/no-array-index-key */
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import {
-  CaretRightIcon,
-  CropIcon,
-  FrameIcon,
-  Link2Icon,
-  PersonIcon,
-} from "@radix-ui/react-icons";
+import { CaretRightIcon, FileIcon } from "@radix-ui/react-icons";
 
 import { VechaiIcon } from "~/components";
-import { configNavigationDropdownMenuItems } from "~/configs";
+import {
+  configNavigationDropdownMenuItems,
+  configNavigationExamples1,
+} from "~/configs";
 import { IconMenu } from "~/libs";
 import { classx } from "~/utils";
 
-import type { HTMLSpanElementProps, RadixDropdownMenuItems } from "~/types";
-
-type User = {
-  name: string;
-  url?: string;
-};
-
-const regionToolMenuItems: RadixDropdownMenuItems = [
-  {
-    text: "Frame",
-    shortcut: "⌘+F",
-    icon: <FrameIcon className="mr-2 h-3.5 w-3.5" />,
-  },
-  {
-    text: "Crop",
-    shortcut: "⌘+S",
-    icon: <CropIcon className="mr-2 h-3.5 w-3.5" />,
-  },
-];
-
-const users: User[] = [
-  { name: "Adam", url: "https://github.com/adamwathan.png" },
-  { name: "Steve", url: "https://github.com/steveschoger.png" },
-  { name: "Robin", url: "https://github.com/robinmalfait.png" },
-];
+import type { HTMLElementProps, HTMLSpanElementProps } from "~/types";
 
 export const NavigationBarDropdownMenu = () => {
   return (
@@ -65,95 +38,33 @@ export const NavigationBarDropdownMenu = () => {
           )}
         >
           {configNavigationDropdownMenuItems.map(
-            ({ text, icon, shortcut }, i) => {
+            ({ to, text, icon, shortcut }) => {
               return (
-                <DropdownMenu.Item
-                  key={`${text}-${i}`}
-                  className={classx(
-                    "navlink navlink-size",
-                    "flex select-none items-center rounded-md text-sm outline-none",
-                  )}
-                >
+                <DropdownMenuItem key={to}>
                   {icon}
                   <span className="flex-grow">{text}</span>
                   {shortcut && <Shortcut>{shortcut}</Shortcut>}
-                </DropdownMenu.Item>
+                </DropdownMenuItem>
               );
             },
           )}
 
           <DropdownMenu.Separator className="bg-separator my-1 h-px" />
 
-          <DropdownMenu.Label
-            className={classx(
-              "navlink-size",
-              "select-none text-sm text-neutral-700 dark:text-neutral-200",
-            )}
-          >
-            Region Tools
-          </DropdownMenu.Label>
-
-          {regionToolMenuItems.map(({ text, icon, shortcut }, i) => {
-            return (
-              <DropdownMenu.Item
-                key={`${text}-${i}`}
-                className={classx(
-                  "navlink navlink-size",
-                  "flex select-none items-center rounded-md text-sm outline-none",
-                )}
-              >
-                {icon}
-                <span className="flex-grow">{text}</span>
-                {shortcut && <Shortcut>{shortcut}</Shortcut>}
-              </DropdownMenu.Item>
-            );
-          })}
-
-          <DropdownMenu.Separator className="bg-separator my-1 h-px" />
-
           <DropdownMenu.Root>
-            <DropdownMenu.TriggerItem
-              className={classx(
-                "radix-state-open:bg-primary-100",
-                "dark:radix-state-open:bg-neutral-700",
-                "navlink navlink-size",
-                "flex w-full select-none items-center rounded-md text-sm outline-none",
-              )}
-            >
-              <Link2Icon className="mr-2 h-3.5 w-3.5" />
-              <span className="flex-grow">Examples</span>
-              <CaretRightIcon className="h-3.5 w-3.5" />
-            </DropdownMenu.TriggerItem>
-            <DropdownMenu.Content
-              className={classx(
-                "origin-radix-dropdown-menu",
-                "radix-side-right:animate-scale-in",
-                "bg-panel shadow-panel border-panel w-full rounded-md px-1 py-1 text-sm",
-              )}
-            >
-              {users.map(({ name, url }, i) => {
+            <DropdownMenuTriggerItem>
+              <FileIcon className="dropdown-menu-icon" />
+              <span className="flex-grow">Examples 1</span>
+            </DropdownMenuTriggerItem>
+            <DropdownMenuContent>
+              {configNavigationExamples1.map(({ to, text }) => {
                 return (
-                  <DropdownMenu.Item
-                    key={`${name}-${i}`}
-                    className={classx(
-                      "navlink navlink-size",
-                      "flex w-28 select-none items-center rounded-md text-sm outline-none md:w-32",
-                    )}
-                  >
-                    {url ? (
-                      <img
-                        alt={name}
-                        className="mr-2.5 h-6 w-6 rounded-full"
-                        src={url}
-                      />
-                    ) : (
-                      <PersonIcon className="mr-2.5 h-6 w-6" />
-                    )}
-                    <span className="">{name}</span>
-                  </DropdownMenu.Item>
+                  <DropdownMenuItem key={to}>
+                    <span>{text}</span>
+                  </DropdownMenuItem>
                 );
               })}
-            </DropdownMenu.Content>
+            </DropdownMenuContent>
           </DropdownMenu.Root>
         </DropdownMenu.Content>
       </DropdownMenu.Root>
@@ -163,4 +74,47 @@ export const NavigationBarDropdownMenu = () => {
 
 export const Shortcut = ({ children }: HTMLSpanElementProps) => {
   return <span className="text-xs text-neutral-500">{children}</span>;
+};
+
+export const DropdownMenuTriggerItem = ({ children }: HTMLElementProps) => {
+  return (
+    <DropdownMenu.TriggerItem
+      className={classx(
+        "navlink navlink-size dropdown-menu-item-focus",
+        "radix-state-open:bg-primary-100",
+        "dark:radix-state-open:bg-neutral-700",
+        "flex w-full select-none items-center rounded-md text-sm outline-none",
+      )}
+    >
+      {children}
+      <CaretRightIcon className="h-3.5 w-3.5" />
+    </DropdownMenu.TriggerItem>
+  );
+};
+
+export const DropdownMenuContent = ({ children }: HTMLElementProps) => {
+  return (
+    <DropdownMenu.Content
+      className={classx(
+        "origin-radix-dropdown-menu",
+        "radix-side-right:animate-scale-in",
+        "bg-panel shadow-panel border-panel w-full rounded-md px-1 py-1 text-sm",
+      )}
+    >
+      {children}
+    </DropdownMenu.Content>
+  );
+};
+
+export const DropdownMenuItem = ({ children }: HTMLElementProps) => {
+  return (
+    <DropdownMenu.Item
+      className={classx(
+        "navlink navlink-size dropdown-menu-item-focus",
+        "flex select-none items-center rounded-md text-sm outline-none",
+      )}
+    >
+      {children}
+    </DropdownMenu.Item>
+  );
 };
