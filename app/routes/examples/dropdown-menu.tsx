59ce1bec39e1f84613a56a49cdf53dfa7ade42bb
M Haidar Hanif <git@mhaidarhanif.com>
2022-06-02 03:11:22 +0700

    feat: route dropdown menu loader data

--- app/routes/examples/dropdown-menu.tsx
+++ app/routes/examples/dropdown-menu.tsx
@@ -1,23 +1,46 @@
+import { json } from "@remix-run/node";
+
 import { DropdownMenu, NavigationBarDropdownMenu } from "~/components";
+import { useLoaderData } from "~/hooks";
 import { Layout } from "~/layouts";
+import { createMetaData } from "~/utils";
+
+import type { MetaFunction, LoaderFunction } from "~/types";
+
+export const loader: LoaderFunction = async () => {
+  return json({
+    title: "Example: Dropdown Menu",
+    description: "Dropdown Menu with Radix UI.",
+  });
+};
+
+export const meta: MetaFunction = ({ data }) => {
+  return createMetaData(data);
+};
 
 export default function ExampleDropdownMenu() {
+  const { title, description } = useLoaderData();
+
   return (
     <Layout>
-      <article className="prose-config">
-        <h1>Example: Dropdown Menu</h1>
-        <p>Dropdown Menu with Radix UI.</p>
+      <header>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </header>
+
+      <article className="example">
+        <h3>Navigation Bar Dropdown Menu</h3>
+        <NavigationBarDropdownMenu />
       </article>
 
       <article className="example">
-        <div className="prose-config h-[600px]">
-          <h2>Dropdown Menu</h2>
-          <DropdownMenu />
-
-          <h2>Navigation Bar Dropdown Menu</h2>
-          <NavigationBarDropdownMenu />
-        </div>
+        <h3>Dropdown Menu from Tailwind CSS Radix</h3>
+        <DropdownMenu />
       </article>
+
+      <div className=" h-[600px]">
+        <small>This is some space to show the popup menu.</small>
+      </div>
     </Layout>
   );
 }
